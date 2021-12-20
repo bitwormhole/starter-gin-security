8bbb41b74a71d58b5b493157efa2206ece8c57e5
比特虫洞 <43868880+bitwormhole@users.noreply.github.com>
2021-12-20 12:12:19 +0800

    添加了 element.DefaultAuthentication

--- gate/element/default_authentication_manager.go
+++ gate/element/default_authentication_manager.go
@@ -1,6 +1,7 @@
 package element
 
 import (
+	"bytes"
 	"context"
 	"errors"
 
@@ -32,3 +33,46 @@ func (inst *DefaultAuthenticationManager) Authenticate(ctx context.Context, a se
 	}
 	return nil, errors.New("bad authentication")
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+// DefaultAuthentication 默认的验证凭据
+type DefaultAuthentication struct {
+	_mechanism string
+	_user      string
+	_secret    []byte
+}
+
+func (inst *DefaultAuthentication) _Impl() security.Authentication {
+	return inst
+}
+
+func (inst *DefaultAuthentication) Mechanism() string {
+	return inst._mechanism
+}
+
+func (inst *DefaultAuthentication) UserID() string {
+	return inst._user
+}
+
+func (inst *DefaultAuthentication) UserSecret() []byte {
+	return inst._secret
+}
+
+func (inst *DefaultAuthentication) SetMechanism(s string) {
+	inst._mechanism = s
+}
+
+func (inst *DefaultAuthentication) SetUserID(s string) {
+	inst._user = s
+}
+
+func (inst *DefaultAuthentication) SetUserSecret(b []byte) {
+	buffer := bytes.Buffer{}
+	if b != nil {
+		buffer.Write(b)
+	}
+	inst._secret = buffer.Bytes()
+}
+
+////////////////////////////////////////////////////////////////////////////////
